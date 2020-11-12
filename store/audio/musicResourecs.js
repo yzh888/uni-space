03a5b81015e2e403a163a6caa81db651a1b3f8d0
yzh888 <1213790892@qq.com>
2020-11-12 14:09:50 +0800

    简单的音乐播放器

---
+++ store/audio/musicResourecs.js
@@ -0,0 +1,30 @@
+export default {
+	musicResourecs: [{
+			id: 10001,
+			name: 'David Osborne - On Golden Pond',
+			src: '/static/mp3/David Osborne - On Golden Pond.mp3',
+			singer: {
+				name: 'yyyan',
+				synopsis: '来自xiaoyan二哥的David Osborne - On Golden Pond,首翻唱自其他歌手的歌曲视频后在YouTube上获得大量的追随者。'
+			}
+		},
+		{
+			id: 10002,
+			name: 'Florian Bur - 就是我',
+			src: '/static/mp3/Florian Bur - Heaven-Snowflakes.mp3',
+			singer: {
+				name: 'Florian Bur - 我唱的',
+				synopsis: '来自xiaoyan二哥的Florian Bur - Heaven-Snowflakes。'
+			}
+		},
+		{
+			id: 10003,
+			name: 'Ive - 風のアルペジオ (风之琶音)',
+			src: '/static/mp3/Ive - 風のアルペジオ (风之琶音).mp3',
+			singer: {
+				name: '小王',
+				synopsis: '中国女歌手徐州优美女高音。'
+			}
+		}
+	]
+}
